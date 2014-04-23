87d5bacad967ba3df2fb32bd3eaf5325097f5fe8
Khash Sajadi <khash@cloud66.com>
2014-04-23 08:09:44 -0400

    don't worry about app secret

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ips::Application.config.secret_key_base = '503b039610fce68be0ad3e331db79742ebca6ec1c992c1654f9977b9318b170af2169d7821b682ba0e8c324933c324b259e573ffdd5ef84548303833ce7c8c5c'
